4a505c526b24269b60a108f7fc261b19c9ff23a8
Ludwing Perez <mirage232@gmail.com>
2019-10-01 16:58:07 -0600

    Implementación de base de datos en tiempo real de firebase para usuarios y habitaciones.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,31 @@
 import Vue from 'vue';
+// firebase SIEMPRE DEBE CARGARSE ANTES DE App.vue!!
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDHnh8f9C2yLaAYdMVOo1r1GZvSbn-1SQs',
+  authDomain: 'p-rooms.firebaseapp.com',
+  databaseURL: 'https://p-rooms.firebaseio.com',
+  projectId: 'p-rooms',
+  storageBucket: '',
+  messagingSenderId: '380158800038',
+  appId: '1:380158800038:web:d02203b21a83624f7f0467',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 new Vue({
   router,
   store,
   render: h => h(App),
+  beforeCreate() {
+    this.$store.dispatch('FETCH_USER', { id: store.state.authId });
+  },
 }).$mount('#app');
